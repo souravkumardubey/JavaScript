bdc1bf418a3132543f162c7baa3d0fb519fa4d9d
Sourav Kumar Dubey <73059672+souravkumardubey@users.noreply.github.com>
2022-04-29 21:26:02 +0530

    Update github.js

--- Project-Github-User-Finder/github.js
+++ Project-Github-User-Finder/github.js
@@ -1,7 +1,9 @@
 class Github {
     constructor () {
-        this.client_id = "";
-        this.client_secret = "";
+        this.client_id = ""; // add client id
+        this.client_secret = ""; // add client secret
+        this.repo_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser( user ) {
@@ -10,12 +12,17 @@ class Github {
         https://api.github.com/users/${user}?client_id = ${this.client_id}& client_secret = ${
             this.client_secret}` )
 
+        const repoResponse = await fetch ( `
+        https://api.github.com/users/${user}/repos?per_page=${this.repo_count}&sort=${this.repos_sort}&client_id = ${this.client_id}& client_secret = ${
+            this.client_secret}` )
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
 
-            profile // profile = profile
-
+            profile ,// profile = profile
+            repos
 
         }
 
